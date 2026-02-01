ba2b8fb0a9d972c7c031bfa1a21b24190dc7c22f
shawnt1790 <shawnreby04@gmail.com>
2026-01-31 21:23:37 -0500

    fixed formatting

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -18,12 +18,12 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-    console.log("socket connected:", socket.id);
-    
-    socket.on("disconnect", (data) => {
-        console.log("socket disconnected:", socket.id, data);
-    });
-})
+  console.log("socket connected:", socket.id);
+
+  socket.on("disconnect", (data) => {
+    console.log("socket disconnected:", socket.id, data);
+  });
+});
 
 const port = Number(process.env.PORT ?? 3001);
 httpServer.listen(port, () => console.log(`server listening on :${port}`));
